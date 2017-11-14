f12143177b73b5e157a8af26b5db502c34b115d8
valygar <florian_culie@yahoo.fr>
2017-11-14 11:36:34 +0100

    Initial commit Contain mockup: 1 - 1a - 1b - 1ca - 1cb - 1d - 5

---
+++ app/src/main/java/fr/utt/if26/fillmyfridge/ListeDeCourseActivity.java
@@ -0,0 +1,13 @@
+package fr.utt.if26.fillmyfridge;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListeDeCourseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_liste_de_course);
+    }
+}
