8768a57645850ddfd49587332e02637ac1e072c4
valygar <florian_culie@yahoo.fr>
2017-11-15 13:43:53 +0100

    End of activity first version

---
+++ app/src/main/java/fr/utt/if26/fillmyfridge/AllListMenuActivity.java
@@ -0,0 +1,13 @@
+package fr.utt.if26.fillmyfridge;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AllListMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_all_list_menu);
+    }
+}
---
+++ app/src/main/java/fr/utt/if26/fillmyfridge/ListMenuDetailsActivity.java
@@ -0,0 +1,13 @@
+package fr.utt.if26.fillmyfridge;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListMenuDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_menu_details);
+    }
+}
