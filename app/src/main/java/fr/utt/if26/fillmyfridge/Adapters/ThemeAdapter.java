30007b84bf81ec811853486201d0a804c6ead2d9
alexandrepa <alexandre.patelli@utt.fr>
2017-11-21 11:44:28 +0100

    Simple spinner alpha

--- app/src/main/java/fr/utt/if26/fillmyfridge/Adapters/ThemeAdapter.java
+++ app/src/main/java/fr/utt/if26/fillmyfridge/Adapters/ThemeAdapter.java
@@ -5,15 +5,21 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.SeekBar;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.thomashaertel.widget.MultiSpinner;
 
+import java.util.ArrayList;
+
 import fr.utt.if26.fillmyfridge.GenerateListMenuMealThemeActivity;
 import fr.utt.if26.fillmyfridge.Objects.Menu;
+import fr.utt.if26.fillmyfridge.Objects.Plat;
 import fr.utt.if26.fillmyfridge.Objects.Repas;
+import fr.utt.if26.fillmyfridge.Objects.Tag;
 import fr.utt.if26.fillmyfridge.R;
 
 /**
@@ -59,7 +65,7 @@ public class ThemeAdapter extends ArrayAdapter<Repas> {
             LayoutInflater inflater = LayoutInflater.from(getContext());
             convertView = inflater.inflate(R.layout.row_gen_theme, parent, false);
             viewHolder.dateLabel = (TextView) convertView.findViewById(R.id.TV_meal_theme_row_label);
-            adapter = new ArrayAdapter<String>(activityContext, android.R.layout.simple_spinner_item);
+            /*adapter = new ArrayAdapter<String>(activityContext, android.R.layout.simple_spinner_item);
             adapter.add("Item1");
             adapter.add("Item2");
             adapter.add("Item3");
@@ -79,9 +85,34 @@ public class ThemeAdapter extends ArrayAdapter<Repas> {
             // set initial selection
             boolean[] selectedItems = new boolean[adapter.getCount()];
             selectedItems[1] = true; // select second item
-            spinner.setSelected(selectedItems);
+            spinner.setSelected(selectedItems);*/
 
+            Spinner spinner = (Spinner) convertView.findViewById(R.id.theme_spinner);
+// Create an ArrayAdapter using the string array and a default spinner layout
+            ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(),
+                    R.array.theme_spinner, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+            spinner.setAdapter(adapter);
 
+            spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
+                    // Your code here
+                    String tagString = adapterView.getItemAtPosition(position).toString();
+                    ArrayList<Plat> plats = new ArrayList<Plat>();
+                    ArrayList<Tag> tags = new ArrayList<Tag>();
+                    int[] tagIds = mContext.getResources().getIntArray(R.array.theme_spinner_id);
+                    tags.add(new Tag(tagIds[position], tagString));
+                    plats.add(new Plat(0,"Plat", null, tags));
+                    repas.setPlats(plats);
+                    repas.setId(1);
+                }
+
+                public void onNothingSelected(AdapterView<?> adapterView) {
+                    return;
+                }
+            });
 
             result=convertView;
 
